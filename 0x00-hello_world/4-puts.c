fa5399d597685da8641ffe138f5cd3fe4c917c6d
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-13 03:47:22 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-/* this code prints a message and returns 0*/
 /* Main : entry point*/
 /* Return : 0*/
 /* Description : printing message*/
-       puts("\"Programming is like building a multilingual puzzle");
+       puts("\"Programming is like building a multilingual puzzle"); [A
        return (0);
+}
