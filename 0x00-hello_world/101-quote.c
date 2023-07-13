76a3a632b3ab23629b7afbb9a4e3edc3a4435089
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-13 04:42:58 -0700

    extra tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ * Description - Printing message followed by a new line
+ */
+int main(void)
+{
+return (1);
+}
