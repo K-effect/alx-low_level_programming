2c9f988188a6ee556032848ab77d23347fdafcdf
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-13 04:20:33 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Description - Printing size of different types
+ */
+int main(void)
+printf("Size of a char: %1d byte(s)\n", sizeof(char));
+printf("Size of an int: %1d byte(s)\n", sizeof(int));
+printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %1d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %1d byte(s)\n", sizeof(float));
+return (0);
+}
