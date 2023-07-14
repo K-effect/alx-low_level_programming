36bb88ab551336d01fbb4fa0d9d30bee8fb51986
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-14 22:43:44 +0200

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,8 +7,10 @@
  */
 int main(void)
 {
-  for (a = 0; a < b; a++)
-    printf("%d\n", a);
-  printf("\n");
+	int a;
+
+	for (a = 0; a < b; a++)
+		printf("%d\n", a);
+	printf("\n");
 	return (0);
 }
