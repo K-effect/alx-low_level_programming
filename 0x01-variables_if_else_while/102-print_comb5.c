54829191ee240b8cdb8bdf7556a8499aba62c6c6
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-14 23:03:20 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 /**
- * main - Prints all possible combinations of a pair of two digit numbers, without any repetition.
+ * main - Prints all possible combinations of a pair 
+ * of two digit numbers, without any repetition.
  *
  * Return: 0 on success.
- * Description: using puchar, print all possible combinations of two two-digit numbers separated by comma, followed by a space.
+ * Description: using puchar, print all possible combinations 
+ * of two two-digit numbers separated by comma, followed by a space.
  */
 
 int main(void)
 {
+	int i;
+	int j;
+	
 	for (i = 0; i <= 99; i++)
 	{
 		for (j = 0; j <= 99; j++)
