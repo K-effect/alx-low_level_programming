c6f20b20231bcb5ae4e3a6648e561a70d127da3f
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-14 10:54:02 +0200

    Update 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 00 to 99.
+ *
+ * Return: 0 on success
+ * Description: using putchar, print the smallest combination of three digits followed by a coma and space
+ */
+int main(void)
+{
+	for( i=0; i <= '7', i++)
+	{
+		for( j=0; j <= '8', j++)
+		{
+			for( k=0; k <= '9', k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+			}
+			k = '0';
+		}
+		j = '0';
+	}
+	putchar('\n');
+	return (0);
+}
