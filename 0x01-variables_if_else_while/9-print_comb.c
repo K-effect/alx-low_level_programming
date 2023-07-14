079f40f013b9a466ee944195418dade49a8f7dc7
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-14 10:38:37 +0200

    Update 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all possible combinations of single-digit numbers.
+ *
+ * Return: 0 on success
+ * Description: Using only putchar, print all possible combination of single-digit numbers separated by ,, followed by a space
+ */
+int main(void)
+{
+	int i = '0'; i <= '9'; ++i)
+	{
+		putchar(i);
+		if (i != '9')
+			putchar(', ');
+	}
+	putchar('\n');
+	return (0);
+}
