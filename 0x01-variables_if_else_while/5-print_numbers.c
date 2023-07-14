cdbf8ad2698060c18e83f8dff7a0b08810ee6424
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-14 10:25:30 +0200

    Update 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0-9.
+ *
+ * Return: 0 on success
+ * Description: print single digit numbers followed by a new line
+ */
+int main(void)
+{
+  for (a = 0; a < b; a++)
+    printf("%d\n", a);
+  printf("\n");
+	return (0);
+}
