c58980f1460a2a7be116339e10b56024a965db9e
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-14 22:46:50 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-
 /**
  * main - Prints numbers from 0-9.
  *
  * Return: 0 on success
- * Description: print single digit numbers followed by a new line using only putchar
+ * Description: print single digit numbers 
+ * followed by a new line using only putchar
  */
 int main(void)
 {
-  for (a = 0; a < b; a++)
-    putchar(a);
-  putchar("\n");
+	int a;
+
+	for (a = 0; a < b; a++)
+		putchar(a);
+	putchar("\n");
 	return (0);
 }
