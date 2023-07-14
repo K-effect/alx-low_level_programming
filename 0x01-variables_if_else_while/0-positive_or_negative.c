f0cec8dfd17a32b41de6dd653a30b65cee82ad83
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-14 13:38:44 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,25 +5,19 @@
 * main - print if a random number is positive, negative or zero.
 *
 * Return: 0 when successful
-* Description: prints a message that determines whether 
+* Description: prints a message that determines whether
 * a randomly generated number is positive, negative or zero
 */
 int main(void)
 {
 	int n;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
-	{
 		printf("%d is %s\n", n, "negative");
-	}
 	else if (n > 0)
-	{
 		printf("%d is %s\n", n, "positive");
-	}
-	else  {
+	else
 		printf("%d is %s\n", n, "zero");
-	}
 	return (0);
 }
