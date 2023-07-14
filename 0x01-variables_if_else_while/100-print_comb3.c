4e33cc041e6f342fc77ed219348f3c4e4f199882
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-14 14:03:31 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,22 +3,21 @@
  * main - print numbers from 00 to 99.
  *
  * Return: 0 on success
- * Description: using putchar, print all the possible smallest combinations of digits between 0 and 9
+ * Description: using putchar, print all the 
+ * possible smallest combinations of digits between 0 and 9
  */
 int main(void)
 {
-	for(i = '0'; i <= '9'; i++)
+	for(int i = '0'; i <= '9'; i++)
 	{
-		for(j = '0'; j <= '9'; j++)
+		for(int j = '0'; j <= '9'; j++)
 		{
 			if (!(i > j || i == j))
 			{
 				putchar(i);
 				putchar(j);
 				if (i == '8' && j == '9')
-				{
 					putchar('\n');
-				}
 				else
 				{
 					putchar(',');
