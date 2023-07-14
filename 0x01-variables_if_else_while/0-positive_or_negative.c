b3562eecd522d57addc5d9d3ff3f8e6963cce12f
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-14 00:06:08 -0700

    positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+main - * main - print if a random number is positive, negative or zero.
+*
+Return: 0 when successful
+Description: prints a message that determines whether a randomly generated number is positive, negative or zero
+int main(void)
+{
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+{
+printf("%d is %s\n", n, "negative");
+}
+else if (n > 0)
+{
+printf("%d is %s\n", n, "positive");
+}
+else
+{
+printf("%d is %s\n", n, "zero");
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+this is the C programming poject for variables, if, else and while
