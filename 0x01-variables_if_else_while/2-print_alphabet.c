989513999707f08fd359fc83674246e555fb4494
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-14 09:55:15 +0200

    Update 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase.
+ *
+ * Return: 0 on success
+ * Description: print the alphabet using puts
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
