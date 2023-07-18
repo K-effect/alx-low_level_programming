5a1dddd39560ca5607163f6f5613acce2f893c3e
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 13:51:00 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,8 +10,8 @@ int main(void)
   char s[] = '_putchar';
   int i;
 
-  for (i = 0; i =< ; i++)
-    putchar(s[i]);
-  putchar('\n');
+  for (i = 0; i =<8 ; i++)
+    _putchar(s[i]);
+  _putchar('\n');
 	return (0);
 }
