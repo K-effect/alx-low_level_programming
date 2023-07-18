d534b229a7d50c4fc6f4a46fbb5e12492af81651
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 14:39:59 +0200

    Update 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _islower - check for lowercase character.
+*
+* Return: 0 when successful
+* Description: check for lowercase character.
+* Returns 1 if c is lowercase
+* Returns 0 otherwise
+*/
+int _islower(int c)
+{
+  if (c >= 97 && c <= 122)
+    return (1)
+    else
+    return (0)
+}
