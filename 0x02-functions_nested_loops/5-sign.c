a28a6628185a7fbf2b7e75e42ff436852af1b684
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 14:54:46 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@
 */
 int print_sign(int n)
 {
-  if (n < 0)
+  if (n > 0)
   {
     _putchar('+');
     return (1);
@@ -22,7 +22,7 @@ int print_sign(int n)
   }
   else
   {
-    _putchar('0');
+    _putchar('-');
     return (-1);
   }
 }
