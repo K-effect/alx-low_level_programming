67d2e479eb69b48df3b16056439a2c67bc70e969
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 14:23:16 +0200

    Update 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - print  the alphbabet
+*
+* Return: 0 when successful
+* Description: prints the alphabet in lower 
+* case followed by a newline
+void print_alphabet(void){
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+  _putchar(a);
+_putchar('/n')
+}
