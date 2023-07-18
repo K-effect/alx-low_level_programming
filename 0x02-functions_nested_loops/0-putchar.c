2f7b6119be75df868d4f2c8dbfae3d6545ea3f5e
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 13:43:35 +0200

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+* main - print  _putchar followed by a new line
+*
+* Return: 0 when successful
+* Description: prints _putchar followed by a new line
+*/
+int main(void)
+{
+  char s[] = '_putchar';
+  int i;
+
+  for (i = 0; i =< ; i++)
+    putchar(s[i]);
+  putchar('\n');
+	return (0);
+}
