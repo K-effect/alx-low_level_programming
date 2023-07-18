9e5af43b209d990939c059fa73dd3ca1732f5e4a
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 20:49:12 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
           _putchar(' ');
           _putchar(prod+'0');
          }
-          if (b != 9)
+          if (b != n)
          {
           _putchar(',');
           _putchar(' ');
