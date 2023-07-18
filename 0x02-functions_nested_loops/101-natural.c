1bc38932e420809f3a00d6aadd7a2694bf5910d6
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 21:10:31 +0200

    Update 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - computes and prints the sum of all the multiples of
+  * 3 or 5 below 1024.
+  *
+  * Return: void
+  * Description: computes and prints the sum of all 
+  * the multiples of 3 or 5 below 1024 (excluded)
+  */
+int main(void)
+{
+	int i, mod;
+
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			mod += i;
+	printf("%d\n", res);
+	return (0);
+}
