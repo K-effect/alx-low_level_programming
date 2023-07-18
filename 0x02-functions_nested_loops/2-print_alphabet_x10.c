01bd9b7f6446a6bc3e31c912182ff9b8d52190ff
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 14:35:32 +0200

    Update 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print the alphabet 10 times
+*
+* Return: 0 when successful
+* Description:  print 10 times the alphabet, 
+* in lowercase, followed by a new line
+*/
+void void print_alphabet_x10(void)
+{
+  char a;
+  int i;
+
+  for (i = 0; i < 10; i++)
+    {
+      for (a = 'a'; a <= 'z'; a++)
+      _putchar(a);
+    }
+   _putchar('\n');
+}
