dfac93163da8f07d00ea0ab084efbb3bd1d187b2
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 15:26:00 +0200

    Update 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * times_table - Print the 9 times table, starting with 0
+  *
+  * Return: empty output
+  */
+void times_table(void)
+{
+  int a, b;
+
+  for (a = 0; a <= 9; a++)
+    {
+      for (a = 0; a <= 9; a++)
+        {
+          prod = a*b;
+          _putchar(prod / 10 + '0');
+          _putchar(prod % 10 + '0');
+          _putchar(',');
+          _putchar(' ');
+        }
+      _putchar('\n');
+    }
+}
