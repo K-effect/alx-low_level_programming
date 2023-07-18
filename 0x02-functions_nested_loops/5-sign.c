0aeddfaeb91ed87b61fa32920283f6d2abe6277f
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 14:52:06 +0200

    Update 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_sign - check for sign of int
+*  @n: The character to be checked
+*
+* Return: 1 when successful
+* Description: Returns 1 and prints + if n is greater than zero
+* Returns 0 and prints 0 if n is zero
+* Returns -1 and prints - if n is less than zero
+*/
+int print_sign(int n)
+{
+  if  n < 0
+    {
+    _putchar('+');
+    return (1);
+    }
+  else if n == 0
+    {
+    _putchar('0');
+    return (0);
+    }
+  else
+    {
+    _putchar('0');
+    return (0);
+    }
+}
