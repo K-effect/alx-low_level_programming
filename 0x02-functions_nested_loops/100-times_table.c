ebc772c7459a9071319533a6f27808debe5cc81f
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 20:49:45 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,7 +31,7 @@ void print_times_table(int n)
             _putchar(' ');
             _putchar(prod+'0');
            }
-            if (b != 9)
+            if (b != n)
            {
             _putchar(',');
             _putchar(' ');
