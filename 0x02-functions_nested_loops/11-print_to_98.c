97fb954fa919be0398661dc0af9f0aec46a64c04
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-18 15:58:02 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,8 +25,8 @@ void print_to_98(int n)
 			_putchar(',');
 			_putchar(' ');
 			}
-		_putchar('\n');
 		}
+		_putchar('\n');
 	}
 	else
 		{
@@ -50,7 +50,7 @@ void print_to_98(int n)
 			_putchar(',');
 			_putchar(' ');
 			}
-		_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
