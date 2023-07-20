dfaf14930e112000f6853645ddd4d4fe221f1624
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-20 11:38:03 +0200

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,13 @@
 void print_diagonal(int n)
 {
 	int i;
+	
 	if ( n > 0)
 	  {
 	    for ( i = 0; i < n; i++)
-	      _putchar(' ');
-	    _putchar('\');
+	      _putchar('\');
 	    _putchar('\n');
 	  }
-	  else
+	else
 	    _putchar('\n');
 }
