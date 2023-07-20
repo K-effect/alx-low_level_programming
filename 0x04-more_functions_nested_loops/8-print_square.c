16fa49b36b6eec4f02031566f10cf570dea5f2a5
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-20 09:10:13 +0200

    Update 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - function that prints a square, followed by a new line.
+ * @size: the size of the square
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+  
+  if ( size > 0)
+  {
+    for ( j = 0; j < size; j++)
+      {
+        for ( i = 0; i < size; i++)
+          _putchar('/');
+        _putchar('\n');
+      }
+  }
+  else
+    _putchar('\n');
+}
