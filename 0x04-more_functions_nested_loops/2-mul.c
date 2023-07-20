549056cd46f50913e55f2e56c10bd982d6d52c00
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-20 08:48:42 +0200

    Update 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: integer 1
+ * @b: integer 1
+ *
+ * Return: product of two integers
+ */
+int mul(int a, int b);
+{
+	return(a*b)
+}
