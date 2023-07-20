e0353eb31e1a78a08efa8409d19ac4fc9ebabd83
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-20 14:16:57 +0200

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,14 +3,14 @@
 #include "main.h"
 /**
  * main - prints the largest prime factor of 612852475143
- * Bwave ICT / Bright Daniel
+ *
  * Return: always 0
  **/
 
 int main(void)
 {
 	unsigned long int i;
-  unsigned long int n = 612852475143;
+	unsigned long int n = 612852475143;
 
 	for (i = 3; i < sqrt( n ); i += 2)
 	{
