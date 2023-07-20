c483b14215b459529f20daa1481a86a7da726306
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-20 09:21:04 +0200

    Update 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - fizz buzz interview
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+  
+  for ( i = 0; i < size; i++)
+    {
+      if (i % 15 == 0)
+        printf("FizzBuzz");
+  		else if (i % 3 == 0)
+  			printf("Fizz");
+  		else if (i % 5 == 0)
+  			printf("Buzz");
+  		else
+  			printf("%i", i);
+      _putchar(' ');
+    }
+  _putchar('\n');
+  return (0);
+}
