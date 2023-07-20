b0a06e4a9ec3fd5f275f3741c548142d0845c9a2
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-20 11:33:42 +0200

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,9 +8,8 @@
 void print_line(int n)
 {
 	int i;
-  
-  for ( i = 0; i < n; i++)
-          _putchar('_');
-    _putchar('\n');
-    }
+	
+	for ( i = 0; i < n; i++)
+		_putchar('_');
+	 _putchar('\n');
 }
