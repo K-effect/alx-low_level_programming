d1cb7eb2fbe57bc5899a5297ddaf1dae54980977
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-24 13:06:50 +0200

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_array - prints n elements of an array 
  * of integers, followed by a new line
@@ -14,7 +14,9 @@ void print_array(int *a, int n)
   
 	for (i = 0; i < n; i ++)
   	{
-  		_putchar(a[i]);
+  		printf("%i", a[i]);
+		if (i != n - 1)
+			printf(", ");
   	}
   	_putchar('\n');
 }
