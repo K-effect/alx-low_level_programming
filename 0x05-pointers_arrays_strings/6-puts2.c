45a7114560d585a2cd7615f541e6ae54bdff0da3
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-24 12:45:20 +0200

    Update 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, 
+ * starting with the first character, followed by a new line
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
