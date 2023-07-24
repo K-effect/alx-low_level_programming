ad1b169b21c7fc5092da66263a247f0a4efb6c04
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-24 12:50:06 +0200

    Update 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int length = 0;
+
+	while (str[length] != '\0')
+		length++;
+  if (length % 2 == 0)
+  {
+    for (i = length/2; i < length; i += 2)
+  	{
+  		_putchar(str[i]);
+  	}
+  	_putchar('\n');
+  }
+  else
+  {
+    for (i = (length-1)/2; i < length; i += 2)
+  	{
+  		_putchar(str[i]);
+  	}
+  	_putchar('\n');
+  }
+}
