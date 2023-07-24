e063c08efe3d82cf9fb6175e5e2b385dba57d145
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-24 11:06:24 +0200

    Update 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to print
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+ 	int temp;
+  int i;
+  int j;
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+	for (i = length - 1; i > j; i--)
+		{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+    j++;
+    }
+}
