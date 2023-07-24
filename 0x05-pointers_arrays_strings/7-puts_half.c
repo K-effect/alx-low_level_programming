ce27ce5ccbde92680adbbaf33ac19ac41f52c80b
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-24 12:56:38 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 		length++;
   if (length % 2 == 0)
   {
-    for (i = length/2; i < length; i ++)
+    for (i = (length/2)-1; i < length; i ++)
   	{
   		_putchar(str[i]);
   	}
@@ -23,7 +23,7 @@ void puts_half(char *str)
   }
   else
   {
-    for (i = (length-2)/2; i < length; i ++)
+    for (i = ((length-1)/2)-1; i < length; i ++)
   	{
   		_putchar(str[i]);
   	}
