63507e1cbd6630132c075356e9a7a78e5144c7ec
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-24 16:08:18 +0200

    Update 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to convert
+ *
+ * Return: int
+*/
+int _atoi(char *s)
+{
+	int i = 0;
+  int neg = 0;
+  int length = 0;
+  int f = 0;
+  int digit = 0;
+  int res;
+
+	while (s[length] != '\0')
+		length++;
+
+	while (i < length && f == 0)
+	{
+		if (s[i] == '-')
+			neg++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (neg % 2)
+				digit = -digit;
+			res = res * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		res = 0;
+	return (res);
+}
