bff5c6e4834848ede6bf17ad47bd09be17277e06
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-26 05:49:58 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @src: string to copy
+ * @des: destination string
+ * @n: number of bytes
+ *
+ * Return: char
+ * */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while ( i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
+}
