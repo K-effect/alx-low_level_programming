8f2e2ef13ef444546a3e49840a6d18a27ebaacb5
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-26 06:44:34 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * leet - change vowels to numbers
+ * @c: input string
+ *
+ * Return: char
+ */
+char *leet(char *c)
+{
+	char let[] = "aeotlAEOTL";
+	char rep[] = "4307143071";
+	int i = 0;
+	int j = 0;
+
+	while (*(c + i) != '\0')
+	{
+		while (j <= 9)
+		{
+			if (*(c + i) == let[j])
+			{
+				*(c + i) = rep[j];
+			}
+			J++;
+		}
+		i++;
+	}
+	return (c);
+}
+
