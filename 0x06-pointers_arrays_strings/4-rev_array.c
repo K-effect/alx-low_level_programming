5ab8669eab30d8ae0dc9b4a0aa5fc2666e383552
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-26 06:03:50 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integer
+ * *a: input array 
+ * *n: dimension of the array
+ *
+ * Return: void 
+ * */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	while ( i < n)
+	{
+		temp = *(a + i);
+		*(a + i) = *(a + n);
+		*(a + n) = temp;
+		n--;
+		i++;
+	}
+}
