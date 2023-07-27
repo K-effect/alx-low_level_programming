31a517182f5620f9d8c37f33ab330aacfc95cc0c
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-27 20:27:29 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -31,3 +31,30 @@ char *cap_string(char *c)
 	}
 	return (c);
 }
+char *cap_string(char *c)
+{
+	int i = 0;
+	int j;
+	int ref[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	while (*(c + i) != '\0')
+	{
+	    if (*(c + i) >= 'a' && *(c + i) <= 'z')
+		{
+    		if (i == 0)
+    			*(c + i) = *(c + i) - 32;
+    		else
+    		{
+    		    j = 0;
+    		    while (j <= 12)
+    		    {
+    		        if (ref[j] == *(c + i - 1))
+    		            *(c + i) = *(c + i) - 32;
+    				j++;
+    			}
+    		}
+		}
+	i++;		
+	}
+	return (c);
+}
