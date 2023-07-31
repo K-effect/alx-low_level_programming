487ec71bce414abd6dc8195aff3206af5d95192e
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-31 08:09:01 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr- checks if c is in s
+ * @s: pointer to check
+ * @c: chartacter to look for
+ *
+ * Return: pointer
+ * */
+char *_strchr(char *s, char c)
+{
+    int check = 0;
+    while(n)
+    {
+        if (s[n-1] == c)
+        {
+            check++;
+            return(s+n-1)
+        }
+        n--;
+    }
+    if (check == 0)
+        return (NULL)
+}
