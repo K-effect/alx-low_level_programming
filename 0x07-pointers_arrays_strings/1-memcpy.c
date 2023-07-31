473ed4d513bba71acc337b0dfc8e4a33a8a12912
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-31 07:55:46 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy- fill copy n bytes of the memory
+ * area pointed
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: number of bytes
+ *
+ * Return: pointer
+ * */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    while(n)
+    {
+        dest[n-1] = src[n-1];
+        n--;
+    }
+}
+
