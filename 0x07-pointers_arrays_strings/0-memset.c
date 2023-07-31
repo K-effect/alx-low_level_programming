fd4966d8ba6d4628c60354ac7dd7a9a3c62b85ff
K-effect <60214340+K-effect@users.noreply.github.com>
2023-07-31 21:06:30 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
- * _memset- fill first n n bytes of the memory
- * area pointed to by s with the constant byte b
- * @s: pointer
- * @n: number of bytes
- * @b: filler char
+ * _memset - fills memory with a constant byte
  *
- * Return: pointer
-*/
+ * @s: pointer to memory area to fill
+ * @n: the number of bytes to fill
+ * @b: constant byte
+ *
+ * Return: a pointer to the memory area @s
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i;
 
-	while ( i < n )
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
-		i++;
 	}
+
 	return (s);
 }
