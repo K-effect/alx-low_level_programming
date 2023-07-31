3b10fdf6c9e993aea41033a48101c9bfec052c04
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-31 08:53:28 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*_strstr - find the first occurrence of needle 
+* in haystack.
+* @haystack: string where the search is made
+* @needle: string searched for
+* 
+* Return: pointer
+**/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
