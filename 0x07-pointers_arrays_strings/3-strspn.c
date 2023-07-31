f02eaa8f7d518e311121c1971256c101215b0666
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-31 08:27:04 -0700

    task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strspn-  gets the length of a prefix substring
+ * @s:
+ * @accept:
+ *
+ * Return: int
+ * */
+
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int res = 0;
+	int i;
+	int n = 0;
+
+	while (s[n])
+	{
+	    i = 0;
+		while (accept[i])
+		{
+			if (s[n] == accept[i])
+			{
+				res++;
+				break;
+			}
+			else if (accept[i+1] == '\0')
+			    return (res);
+			i++;
+		}
+		n++;
+	}
+	return (res);
+}
+
