d7648aa6b7b84babdfec017e9b71ab9c685f77e3
k-effect <kaoutar.elotmani2@gmail.com>
2023-07-31 07:43:18 -0700

    first commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset- fill first n n bytes of the memory
+ * area pointed to by s with the constant byte b
+ * @s: pointer
+ * @n: number of bytes
+ * @b: filler char
+ *
+ * Return: pointer
+ * */
+char *_memset(char *s, char b, unsigned int n)
+{
+    int i = 0;
+    while (i < n)
+    {
+        *(s+i) = b ;
+        i++;
+    }
+    return(s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is tjhe repository for the pointers, arrays and strings project in the C programming course
