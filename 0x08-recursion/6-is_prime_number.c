89ae2751b6b303fea8701add52ce003a72637ad2
k-effect <kaoutar.elotmani2@gmail.com>
2023-08-02 06:28:33 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * check_prime - checks if x is prime
+ * @x: input number
+ * @y: number to check
+ *
+ * Return: int
+ */
+int check_prime(int x, int y)
+{
+	int r = 1;
+	
+	if (y < x)
+	{
+		if (y * y == x)
+		{
+			r = 0;
+			y = x;
+		}
+		else
+		{
+			r = check_prime(x, y + 1);
+		}
+	}
+	return (r);
+}
+/**
+ * is_prime_number - check if number is prime
+ * @n: inpput int
+ *
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	int r = 2;
+	
+	if (n > 1)
+	{
+    		return (check_prime( n, r));
+	}
+	else
+    		return (0);
+}
