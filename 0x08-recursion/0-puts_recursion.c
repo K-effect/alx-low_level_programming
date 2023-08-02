b93cfa8aebf8bd6803a86e4f2657d460f6d5f3fe
k-effect <kaoutar.elotmani2@gmail.com>
2023-08-02 03:39:00 -0700

    task 0 4

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * _puts_recursion: prints a string, followed by a new line
+ * _puts_recursion - prints a string, followed by a new line
  * @s: string to print
  *
  * Return: void
- * */
+ */
 void _puts_recursion(char *s)
 {
 	if (*s)
-        {
-                _putchar(*s);
+	{
+		_putchar(*s);
 		s++;
 		_puts_recursion(s);
 	}
 	else
 	{
 		_putchar('\n');
-                return;
-        }
+		return;
+	}
 }
