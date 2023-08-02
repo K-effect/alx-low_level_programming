37944b0c04a1e5657b5409d888af29b39d212ff8
k-effect <kaoutar.elotmani2@gmail.com>
2023-08-02 03:52:26 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: input string
+ *
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+	if (*s)
+	{
+		length = 1 +  _strlen_recursion(s+1);
+	}
+	return (length);
+}
