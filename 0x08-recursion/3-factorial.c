ac914360e4844f62bfda874fa1b57c205d302490
k-effect <kaoutar.elotmani2@gmail.com>
2023-08-02 04:08:07 -0700

    task 3 0

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,8 +7,13 @@
  */
 int factorial(int n)
 {
-	if (n)
-		return(n * factorial(n-1));
+	if ( n > 0 )
+	{
+		if (n)
+	                return( n * factorial(n - 1) );
+        	else
+                	return (1);
+	}
 	else
-		return (1);
+		return (-1);
 }
