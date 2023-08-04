87298daa3d832795a7bf4a26eb22cfd12004142b
k-effect <kaoutar.elotmani2@gmail.com>
2023-08-04 06:07:25 -0700

    file 4

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int c = 0;
+
+	while (c < argc)
+	{
+		c++;
+		printf("%s\n", argv[c]);
+	}
+	return (0);
+}
