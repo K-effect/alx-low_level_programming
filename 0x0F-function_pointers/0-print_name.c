e3cb8779e2553e8ca3c48851da4b7e77b2e34810
k-effect <kaoutar.elotmani2@gmail.com>
2023-08-16 15:19:37 -0700

    ok1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,11 @@
  * print_name - prints a name
  * @name: the name to print
  * @f: the function to send the name to
+ *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
+
 	(*f)(name);
 }
