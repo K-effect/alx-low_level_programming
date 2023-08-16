5d88d0237430679f8f689deb22e55df2dd2c9a62
k-effect <kaoutar.elotmani2@gmail.com>
2023-08-16 15:55:16 -0700

    trial2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,6 +3,8 @@
   * print_name - prints a name
   * @name: input string
   * @f: function pointer casted to char pointer
+  *
+  * Return: void
   */
 void print_name(char *name, void (*f)(char *))
 {
