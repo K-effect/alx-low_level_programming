e8c538c0e23a18bd63cc8c0e74d5a2aeb9837379
k-effect <kaoutar.elotmani2@gmail.com>
2023-08-29 13:20:20 -0700

    101

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,6 +7,20 @@
   */
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *i;
+	listint_t *j;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	j = NULL;
+	while ((*head)->next != NULL)
+	{
+		i = (*head)->next;
+		(*head)->next = j;
+		j = *head;
+		*head = i;
+	}
+	(*head)->next = j;
+
+	return (*head);
 }
