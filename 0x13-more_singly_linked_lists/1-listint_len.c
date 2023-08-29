5457d034a7740330c7db316be6147edf025a265d
k-effect <kaoutar.elotmani2@gmail.com>
2023-08-29 05:30:46 -0700

    1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_listint_len - returns the number of elements
+ * listint_len - returns the number of elements
  * in a linked listint_t list
  * @h: the head of the list to print from
  *
@@ -9,7 +9,7 @@
 size_t listint_len(const listint_t *h)
 {
 	size_t s;
-    
+
 	s = 0;
 	while (h)
 	{
