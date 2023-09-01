d356a9188f6d4a9ac63ead89e67aa7e2b3c90702
k-effect <kaoutar.elotmani2@gmail.com>
2023-09-01 07:27:41 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,6 +10,6 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int res;
 
-	res = (index > (8 * sizeof(n) - 1)) ? -1 : (int)(n >> index) & 1;
-	return((res);
+	res = ((index > (8 * sizeof(n) - 1)) ? -1 : (int)(n >> index) & 1;
+	return(res);
 }
