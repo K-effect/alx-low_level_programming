1c14099d5c027759fb84433fd6abb93d5d1afeda
k-effect <kaoutar.elotmani2@gmail.com>
2023-09-08 18:21:21 -0700

    task 0

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,8 @@
-#include <main.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <elf.h>
+#include <fcntl.h>
 #include "main.h"
 /**
  * read_elf: Reads an ELF header from a file descriptor.
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,3 +10,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void print_error(int file_des);
+void read_elf(int fd, Elf64_Ehdr *header);
+void validate_elf(Elf64_Ehdr *header);
+void display_elf(Elf64_Ehdr *header);
