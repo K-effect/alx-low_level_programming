6eae853cc41bf0572b088a3e261c270a9afdb16e
k-effect <kaoutar.elotmani2@gmail.com>
2023-10-26 03:57:15 -0700

    ok

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,13 +8,8 @@
   */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int temp;
-
-	temp = 8 * sizeof(*n) - 1;
-
-	if (index > temp)
-		return (-1);
-	*n = *n | (1 << index);
-	
-	return (1);
+	if (index >= sizeof(unsigned long int) * 8)
+		return -1;
+    	*n |= (1UL << index);
+    	return 1;
 }
