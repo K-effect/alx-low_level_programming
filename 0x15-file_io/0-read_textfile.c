bab0e4c40c979c572dec2bd9ba8c2a2d036c7d48
k-effect <kaoutar.elotmani2@gmail.com>
2023-10-30 08:09:22 -0700

    ok

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,38 +6,30 @@
  *
  * Return: The actual number of letters read and printed, 0 on failure.
  */
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL) {
-        return 0;
-    }
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-    int file_descriptor = open(filename, O_RDONLY);
+	if (!filename)
+		return (0);
 
-    if (file_descriptor == -1) {
-        return 0;
-    }
+	fd = open(filename, O_RDONLY);
 
-    char *buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL) {
-        close(file_descriptor);
-        return 0;
-    }
+	if (fd == -1)
+		return (0);
 
-    ssize_t bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1) {
-        free(buffer);
-        close(file_descriptor);
-        return 0;
-    }
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    free(buffer);
-    close(file_descriptor);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-    if (bytes_written == -1 || bytes_written != bytes_read) {
-        return 0;
-    }
+	close(fd);
 
-    return bytes_written;
+	free(buf);
+
+	return (nwr);
 }
-
