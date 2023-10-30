5e425c7742b698088d2e21ebe076514a39effaa1
k-effect <kaoutar.elotmani2@gmail.com>
2023-10-30 07:18:57 -0700

    ok

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,12 @@
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -13,3 +15,5 @@ void print_error(int file_des);
 void read_elf(int fd, Elf64_Ehdr *header);
 void validate_elf(Elf64_Ehdr *header);
 void display_elf(Elf64_Ehdr *header);
+
+#endif /* MAIN_H */
