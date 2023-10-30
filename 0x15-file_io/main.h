2b40f128cc4d6457ff28b6479b88d452314f11fc
k-effect <kaoutar.elotmani2@gmail.com>
2023-10-30 08:39:00 -0700

    ok

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,8 +13,5 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void display_elf(Elf64_Ehdr *header);
-void validate_elf(Elf64_Ehdr *header);
-void read_elf(int fd, Elf64_Ehdr *header);
 
 #endif /* MAIN_H */
