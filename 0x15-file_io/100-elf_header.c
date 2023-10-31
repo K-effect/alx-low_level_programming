d04866d05d7ac57887674046d978b1219e33f8ab
k-effect <kaoutar.elotmani2@gmail.com>
2023-10-31 00:50:28 -0700

    ok

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -45,26 +45,23 @@ void print_elf_header(const Elf64_Ehdr *header)
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-        print_error("Usage: elf_header elf_filename");
+	int fd;
 
-    int fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-        print_error("Could not open the file");
-
-    Elf64_Ehdr header;
-    if (read(fd, &header, sizeof(header)) != sizeof(header))
-        print_error("Error reading ELF header");
-
-    if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-        header.e_ident[EI_MAG1] != ELFMAG1 ||
-        header.e_ident[EI_MAG2] != ELFMAG2 ||
-        header.e_ident[EI_MAG3] != ELFMAG3)
-        print_error("Not an ELF file");
-
-    print_elf_header(&header);
-
-    close(fd);
-    return 0;
+	if (argc != 2)
+		print_error("Usage: elf_header elf_filename");
+	
+    	fd = open(argv[1], O_RDONLY);
+    	if (fd == -1)
+		print_error("Could not open the file");
+    	Elf64_Ehdr header;
+    	if (read(fd, &header, sizeof(header)) != sizeof(header))
+		print_error("Error reading ELF header");
+    	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 || header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
+		print_error("Not an ELF file");
+	
+    	print_elf_header(&header);
+    
+	close(fd);
+    	return(0);
 }
 
